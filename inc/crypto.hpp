1efa916e9c5fee9a46aa7e89fbfdcacc7f7f91c9
generalsuslik <glebrusyaev77@gmail.com>
2025-01-25 19:00:51 +0300

    Added crypto rsa key pair generation

---
+++ inc/crypto.hpp
@@ -0,0 +1,13 @@
+//
+// Created by generalsuslik on 25.01.25.
+//
+
+#ifndef CRYPTO_HPP
+#define CRYPTO_HPP
+
+#include <string>
+#include <utility>
+
+std::pair<std::string, std::string> generate_rsa_keys();
+
+#endif //CRYPTO_HPP
