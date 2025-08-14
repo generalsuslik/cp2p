bb8f07d7478a86e04062c4991845df8038be9bd1
generalsuslik <glebrusyaev77@gmail.com>
2025-08-14 21:17:12 +0300

    Fixed tests

--- test/crypto/test_aes.cpp
+++ test/crypto/test_aes.cpp
@@ -6,6 +6,8 @@
 
 #include "crypto/aes.hpp"
 
+#include "util/util.hpp"
+
 #include "test_util/test_util.hpp"
 
 TEST(AES, test_encryption_decryption) {
@@ -14,7 +16,7 @@ TEST(AES, test_encryption_decryption) {
     const std::string plaintext = random_string();
 
     const std::vector<unsigned char> ciphertext = cp2p::aes::aes_encrypt(plaintext, aes_key, aes_iv);
-    const std::string decrypted = cp2p::aes::aes_decrypt(ciphertext, aes_key, aes_iv);
+    const std::string decrypted = cp2p::get_string_from_container(cp2p::aes::aes_decrypt(ciphertext, aes_key, aes_iv));
 
     assert(plaintext == decrypted);
 }
